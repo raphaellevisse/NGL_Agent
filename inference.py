cap projection scaling, fix action tensor issue

inference.py
@@ -1,6 +1,5 @@
 from Agent import Agent
 from ActorCritic import ActorNetwork
-import time
 import torch
 from torchvision import transforms
 from Values import Values
@@ -21,7 +20,7 @@ discrete_action_indices = [0, 1, 2, 5, 6, 7, 8]
 
 # Load model with weights
 model = ActorNetwork(discrete_dim=7, continuous_dim=11, image_width=480, image_height=270)
-model.load_state_dict(torch.load("./checkpoints/actor_weights_final_v1.pt"))
+model.load_state_dict(torch.load("./checkpoints/actor_weights_final_v2.pt"))
 model.eval()
 
 
@@ -42,7 +41,9 @@ def preprocess_state(state):
     position[1] / values.position_y_factor,
     position[2] / values.position_z_factor,
     ]
-    norm_crossSectionScale = crossSectionScale / values.crossSectionScale_factor
+    norm_crossSectionScale = crossSectionScale /(values.crossSectionScale_factor)
+    #print(values.crossSectionScale_factor)
+
     norm_projectionOrientation = [
     projectionOrientation[0] / values.projectionOrientation_q1_factor,
     projectionOrientation[1] / values.projectionOrientation_q2_factor,
@@ -50,12 +51,15 @@ def preprocess_state(state):
     projectionOrientation[3] / values.projectionOrientation_q4_factor,
     ]
     norm_projectionScale = projectionScale / values.projectionScale_factor
+
+    print("projection section scale"+ str(projectionScale))
     state_vector = norm_position + [norm_crossSectionScale] + norm_projectionOrientation + [norm_projectionScale]
     return torch.tensor(state_vector, dtype=torch.float32).unsqueeze(0).to("cpu")
 
 # build output vector
 def build_output_vector(discrete_actions, continuous_actions):
     # set the maximum value to 1 and the rest to 0
+    print("action probabilities")
     
     discrete_actions = (discrete_actions == discrete_actions.max()).float()
     discrete_list = discrete_actions.cpu()[0]
@@ -63,8 +67,7 @@ def build_output_vector(discrete_actions, continuous_actions):
 
     output_vector = torch.tensor([0.0] * action_size, dtype=torch.float32).to("cpu")
     #print(len(output_vector))
-    #print(discrete_list)
-    #print(continuous_list)
+    
     for i, idx in enumerate(discrete_action_indices):
         value = discrete_list[i]
         
@@ -83,7 +86,7 @@ agent.chrome_ngl.start_neuroglancer_session()
 
 #agent.chrome_ngl.get_screenshot("./screenshot.png")
 num_episodes = 1
-max_steps = 100
+max_steps = 200
 #target_update_freq = 10 
 
 for episode in range(num_episodes):
@@ -107,6 +110,10 @@ for episode in range(num_episodes):
 
         state_tensor = preprocess_state(pos_state)
         image_tensor = preprocess_image(resize_image)
+
+        print("raw inputs")
+        print(state_tensor)
+        print(pos_state)
 
         with torch.no_grad():
             discrete_probs, continuous_probs = model(state_tensor, image_tensor)

stats_episode.py
@@ -9,7 +9,7 @@ from PIL import Image
 def load_episode_data(num_episodes, episodes_path):
     all_episodes = []
     for idx in range(0, num_episodes):
-        episode_path = f"{episodes_path}/episode_{idx}/data.json"
+        episode_path = f"{episodes_path}/episode_{idx}/data_reparsed.json"
         screenshots_path = f"{episodes_path}/episode_{idx}/screenshots"
         images = []
         with open(episode_path, "r") as f:
@@ -28,7 +28,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Using device: {device}", flush=True)
 #agent = Agent(model, start_session=False)
 
-episodes_path = "./parsed_episodes/"
+episodes_path = "./reparsed_episodes/"
 num_episodes = 13
 # This could be done elsewhere but it is sufficiently fast to be done directly here
 episodes_data = load_episode_data(num_episodes, episodes_path)
@@ -37,7 +37,6 @@ all_actions = []
 print(len(episodes_data))
 # Iterate through all episodes
 for episode in episodes_data:
-    # Extract action vector and move it to the device
     action = torch.tensor(episode['action_vector'], dtype=torch.float32).to(device)
     
     # Append to the list for global statistics
@@ -59,17 +58,17 @@ print(f"Mean Action: {mean_action}")
 print(f"Variance Action: {variance_action}")
 
 
-# Max Action: tensor([1.0000e+00, 1.0000e+00, 0.0000e+00, 9.6800e-01, 6.9300e-01, 0.0000e+00,
-#         0.0000e+00, 0.0000e+00, 1.0000e+00, 4.8212e+03, 1.1563e+03, 4.0000e+00,
-#         5.1335e+01, 2.0762e-01, 2.4648e-01, 2.1690e-01, 2.1872e-01, 0.0000e+00])
+# Max Action: tensor([1.0000e+00, 1.0000e+00, 1.0000e+00, 1.4520e+03, 6.9300e+02, 0.0000e+00,
+#         0.0000e+00, 0.0000e+00, 1.0000e+00, 2.0969e+01, 1.1563e+03, 4.0000e+00,
+#         2.3872e+00, 2.0762e-01, 2.4648e-01, 2.1690e-01, 2.1872e-01, 3.7135e+03])
 # Min Action: tensor([ 0.0000e+00,  0.0000e+00,  0.0000e+00,  0.0000e+00,  0.0000e+00,
-#          0.0000e+00,  0.0000e+00,  0.0000e+00,  0.0000e+00, -4.3635e+03,
-#         -1.0524e+03, -4.1279e+00, -5.0272e+01, -2.1178e-01, -2.9298e-01,
-#         -2.0002e-01, -2.1217e-01,  0.0000e+00])
-# Mean Action: tensor([ 3.0797e-02,  4.5652e-02,  0.0000e+00,  4.6422e-02,  2.8594e-02,
-#          0.0000e+00,  0.0000e+00,  0.0000e+00,  9.2355e-01, -3.2228e-01,
-#          1.2004e+00,  2.9492e-01,  1.4930e-02, -1.4409e-03,  8.3225e-04,
-#         -7.2133e-04,  1.6138e-03,  0.0000e+00])
-# Variance Action: tensor([2.9859e-02, 4.3584e-02, 0.0000e+00, 3.0451e-02, 1.1161e-02, 0.0000e+00,
-#         0.0000e+00, 0.0000e+00, 7.0630e-02, 1.8515e+04, 1.7925e+03, 9.4216e-01,
-#         2.3326e+00, 5.8702e-04, 7.5714e-04, 1.0054e-03, 9.2021e-04, 0.0000e+00])
+#          0.0000e+00,  0.0000e+00,  0.0000e+00,  0.0000e+00, -4.4743e+02,
+#         -3.6109e+01, -2.0000e+00,  0.0000e+00, -2.1178e-01, -2.9298e-01,
+#         -2.0002e-01, -1.5845e-01, -8.7997e-01])
+# Mean Action: tensor([ 9.3371e-04,  5.6489e-02,  2.1008e-02,  7.5114e+01,  2.8972e+01,
+#          0.0000e+00,  0.0000e+00,  0.0000e+00,  9.2157e-01, -9.2419e-01,
+#          1.4950e+00,  2.2389e-01,  5.1557e-03, -2.3375e-03,  8.3476e-04,
+#         -6.9265e-04,  2.4668e-03,  1.7921e+00])
+# Variance Action: tensor([9.3327e-04, 5.3323e-02, 2.0577e-02, 7.5191e+04, 1.0959e+04, 0.0000e+00,
+#         0.0000e+00, 0.0000e+00, 7.2314e-02, 1.8582e+02, 1.0954e+03, 6.4959e-01,
+#         5.2019e-03, 6.5645e-04, 8.6409e-04, 1.0306e-03, 9.5943e-04, 6.4426e+03])

train.py
@@ -3,15 +3,31 @@ from ActorCritic import ActorCriticModel
 import time
 import torch
 import os
-torch.autograd.set_detect_anomaly(True)
+#torch.autograd.set_detect_anomaly(True)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Using device: {device}")
 
-model = ActorCriticModel(state_size=10, action_size=18, device=device, batch_size=128) # 10 for 9 values and the image, 18 for 18 possible actions
+model = ActorCriticModel(state_size=10, action_size=18, device=device) # 10 for 9 values and the image, 18 for 18 possible actions
+actor_weights = "weights/actor_weights_best_v5.pt"
+# critic_weights = "weights/critic_weights_best_v5.pt"
+if not os.path.exists(actor_weights):
+     raise FileNotFoundError(f"Actor weights file not found: {actor_weights}")
+# if not os.path.exists(critic_weights):
+#     raise FileNotFoundError(f"Critic weights file not found: {critic_weights}")
+# if os.path.exists(actor_weights) and os.path.exists(critic_weights):
+#     model.load_model(actor_weights, critic_weights)
+#     print("Model loaded successfully.")
+model.actor.load_state_dict(torch.load(actor_weights, map_location=device, weights_only=True))
+#model.critic.load_state_dict(torch.load(critic_weights, map_location=device, weights_only=True))
+model.actor.eval()
+model.critic.eval()
+model.target_actor.eval()
+model.target_critic.eval()
 
 agent = Agent(headless=False, start_session=True)
 agent.chrome_ngl.start_neuroglancer_session()
 #print(agent.chrome_ngl.get_url())
+agent.chrome_ngl.change_url("http://localhost:8000/client/#!%7B%22dimensions%22:%7B%22x%22:%5B4e-9%2C%22m%22%5D%2C%22y%22:%5B4e-9%2C%22m%22%5D%2C%22z%22:%5B4e-8%2C%22m%22%5D%7D%2C%22position%22:%5B138657.265625%2C80856.6953125%2C1335.916015625%5D%2C%22crossSectionScale%22:4.45933655284782%2C%22projectionOrientation%22:%5B0.09884308278560638%2C0.9041123986244202%2C-0.4155852496623993%2C0.009988739155232906%5D%2C%22projectionScale%22:12029.259719517953%2C%22layers%22:%5B%7B%22type%22:%22image%22%2C%22source%22:%22precomputed://https://bossdb-open-data.s3.amazonaws.com/flywire/fafbv14%22%2C%22tab%22:%22source%22%2C%22name%22:%22Maryland%20%28USA%29-image%22%7D%2C%7B%22type%22:%22segmentation%22%2C%22source%22:%22precomputed://gs://flywire_v141_m783%22%2C%22tab%22:%22source%22%2C%22segments%22:%5B%22%21720575940623044103%22%2C%22%21720575940612843473%22%2C%22720575940641265549%22%2C%22720575940625693080%22%2C%22720575940645528430%22%2C%22720575940622572010%22%5D%2C%22name%22:%22flywire_v141_m783%22%7D%5D%2C%22showDefaultAnnotations%22:false%2C%22selectedLayer%22:%7B%22size%22:350%2C%22visible%22:true%2C%22layer%22:%22flywire_v141_m783%22%7D%2C%22layout%22:%22xy-3d%22%7D")
 #print('Saving screenshot...')
 
 #agent.chrome_ngl.get_screenshot("./screenshot.png")
@@ -20,34 +36,39 @@ max_steps = 128
 target_update_freq = 10 
 
 for episode in range(num_episodes):
-    agent.reset()
+    #agent.reset()
+    # Setting the agent to a state it has trained on to see his performance on seen environments.
+    agent.chrome_ngl.change_url("http://localhost:8000/client/#!%7B%22dimensions%22:%7B%22x%22:%5B4e-9%2C%22m%22%5D%2C%22y%22:%5B4e-9%2C%22m%22%5D%2C%22z%22:%5B4e-8%2C%22m%22%5D%7D%2C%22position%22:%5B138657.265625%2C80856.6953125%2C1335.916015625%5D%2C%22crossSectionScale%22:4.45933655284782%2C%22projectionOrientation%22:%5B0.09884308278560638%2C0.9041123986244202%2C-0.4155852496623993%2C0.009988739155232906%5D%2C%22projectionScale%22:12029.259719517953%2C%22layers%22:%5B%7B%22type%22:%22image%22%2C%22source%22:%22precomputed://https://bossdb-open-data.s3.amazonaws.com/flywire/fafbv14%22%2C%22tab%22:%22source%22%2C%22name%22:%22Maryland%20%28USA%29-image%22%7D%2C%7B%22type%22:%22segmentation%22%2C%22source%22:%22precomputed://gs://flywire_v141_m783%22%2C%22tab%22:%22source%22%2C%22segments%22:%5B%22%21720575940623044103%22%2C%22%21720575940612843473%22%2C%22720575940641265549%22%2C%22720575940625693080%22%2C%22720575940645528430%22%2C%22720575940622572010%22%5D%2C%22name%22:%22flywire_v141_m783%22%7D%5D%2C%22showDefaultAnnotations%22:false%2C%22selectedLayer%22:%7B%22size%22:350%2C%22visible%22:true%2C%22layer%22:%22flywire_v141_m783%22%7D%2C%22layout%22:%22xy-3d%22%7D")
+    #print('Saving screenshot...')
     model.memory.clear()
     #pos_state, image, json_state = agent.prepare_state(verbose=True)
     total_reward = 0
 
     for step in range(max_steps):
         print(f"Episode {episode + 1}/{num_episodes}, Step {step + 1}/{max_steps}")
-        # Select action
-        #print("Making decision...")
-        pos_state, curr_image, json_state = agent.prepare_state()
-
-        discrete_probs, continuous_probs = model.action(pos_state, curr_image)
-        #print("Probs are", discrete_probs, continuous_probs)
+        # 1. Interact with the environment and collect data
+        pos_state, curr_image, json_state = agent.prepare_state(image_width=480, image_height=270)
+        #discrete_probs, continuous_probs = model.action(pos_state, curr_image, eval=True)
+        #lets go directly with the actor
+        pos_state_tensor = model.preprocess_state(pos_state)
+        curr_image_tensor = model.preprocess_image(curr_image)
+        #print("Pos state tensor ", pos_state_tensor)
+        #print("Curr image tensor ", curr_image_tensor.shape)
+        discrete_probs, continuous_probs = model.actor(pos_state_tensor, curr_image_tensor)
         output_vector = model.build_output_vector(discrete_probs, continuous_probs)
-        agent.apply_actions(output_vector, json_state) # the ouput vector will either do a click or shift the view via the json state
+        agent.apply_actions(output_vector, json_state) # the output vector will either do a click or shift the view via the json state
 
-        action_probs = [discrete_probs, continuous_probs]
-        next_pos_state, next_image, next_json_state = agent.prepare_state()
-        reward = model.reward(next_json_state)
+        # next_pos_state, next_image, next_json_state = agent.prepare_state()
+        # reward = model.reward(next_json_state)
 
-        done = False
-        model.store_experience(pos_state, curr_image, action_probs, reward, next_pos_state, next_image, done)
+        # done = False
+        # model.store_experience(pos_state, curr_image, discrete_probs, continuous_probs, reward, next_pos_state, next_image, done)
         
-        pos_state = next_pos_state
-        image = next_image
-        total_reward += reward
-
-        
+        # pos_state = next_pos_state
+        # image = next_image
+        # total_reward += reward
+    print(f"Episode {episode + 1}/{num_episodes}, Total Reward: {total_reward}, Epsilon: {model.epsilon}")
+    continue
     print("Training model at end of episode...")
     model.train()
     
@@ -58,6 +79,6 @@ for episode in range(num_episodes):
 
     if (episode+1) % 50 == 0:
         model.save_model(f"./checkpoints/train_actor_weights_episode_{episode+1}.pt", f"./checkpoints/train_critic_weights_episode_{episode+1}.pt")
-    print(f"Episode {episode + 1}/{num_episodes}, Total Reward: {total_reward}, Epsilon: {model.epsilon}")
+    
 
 time.sleep(100)